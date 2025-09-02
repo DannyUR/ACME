b94ceca2079583847accb0c3224b5c24acde9339
DannyUR <111530669+DannyUR@users.noreply.github.com>
2025-09-02 16:35:13 -0600

    Creación de la base de datos
    Creación de las tablas

---
+++ acme/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arqui-ACME</title>
+</head>
+<body>
+    <h1>Bienvenido a la página del Arqui xD</h1>
+</body>
+</html>
+
+
